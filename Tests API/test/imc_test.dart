54dcaf898cf2299b37f86f5e2eae405ebb73bf93
GustavoBorgesSouza <sgustavo.borges10@gmail.com>
2023-08-22 14:49:12 -0300

    changed - grouped tests

--- Tests API/test/imc_test.dart
+++ Tests API/test/imc_test.dart
@@ -8,26 +8,13 @@ void main() {
     expect(result, equals(20.199469763918696));
   });
 
-  // se criarmos na mão podemos fazer assim
-  // if (calculateIMC(1.78, 64) == 20.199469763918696) {
-  //   print("OK: ");
-  // } else {
-  //   print("ERROR: Must calculate IMC");
-  // }
+  group('Parameters Exceptions -', () {
+    test('Must throw exception if heigth is smaller than 1', () {
+      expect(() => calculateIMC(0, 64), throwsA(isA<Exception>()));
+    });
 
-  test('Must throw exception if heigth is smaller than 1', () {
-    expect(() => calculateIMC(0, 64), throwsA(isA<Exception>()));
-  });
-
-  // se criarmos na mão podemos fazer assim
-  // try {
-  //   calculateIMC(0, 64);
-  //   print("ERROR: Must throw exception if heigth is smaller than 1");
-  // } catch (e) {
-  //   print("OK: Must throw exception if heigth is smaller than 1");
-  // }
-
-  test('Must throw exception if weigth is smaller than 1', () {
-    expect(() => calculateIMC(1.78, 0), throwsA(isA<Exception>()));
+    test('Must throw exception if weigth is smaller than 1', () {
+      expect(() => calculateIMC(1.78, 0), throwsA(isA<Exception>()));
+    });
   });
 }
