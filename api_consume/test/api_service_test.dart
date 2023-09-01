09d8c2039c2d7c2a05ce9c55f16cf1a0844b4339
GustavoBorgesSouza <sgustavo.borges10@gmail.com>
2023-09-01 09:08:00 -0300

    added - mocks and stubs with packages

--- api_consume/test/api_service_test.dart
+++ api_consume/test/api_service_test.dart
@@ -2,49 +2,23 @@ import 'dart:async';
 
 import 'package:api_consume/api_service.dart';
 import 'package:api_consume/product.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import 'package:uno/uno.dart';
 
-class UnoMock implements Uno {
-  final bool withError;
+// mock creation here
+class UnoMock extends Mock implements Uno {}
 
-  UnoMock([this.withError = false]);
-
-  @override
-  Future<Response> get(String url,
-      {Duration? timeout,
-      Map<String, String> params = const {},
-      Map<String, String> headers = const {},
-      ResponseType responseType = ResponseType.json,
-      DownloadCallback? onDownloadProgress,
-      ValidateCallback? validateStatus}) async {
-    if (withError) {
-      throw UnoError('error');
-    }
-
-    return Response(
-      headers: headers,
-      request: Request(
-        headers: {},
-        method: '',
-        timeout: Duration(seconds: 1),
-        uri: Uri.base,
-      ),
-      status: 200,
-      data: productsListJson,
-    );
-  }
-
-  // tudo chamado que não foi implementado cai aqui
-  @override
-  noSuchMethod(Invocation invocation) {
-    return super.noSuchMethod(invocation);
-  }
-}
+class ResponseMock extends Mock implements Response {}
 
 void main() {
   test('Must return a list of products', () {
     final uno = UnoMock();
+    final response = ResponseMock();
+
+    // stubs config here
+    when(() => response.data).thenReturn(productsListJson);
+    when(() => uno.get(any())).thenAnswer((_) async => response);
     final service = APIService(uno);
 
     expect(
@@ -56,7 +30,10 @@ void main() {
     );
   });
   test('Must return an empty list of products when there is an error', () {
-    final uno = UnoMock(true);
+    final uno = UnoMock();
+
+    when(() => uno.get(any())).thenThrow(UnoError('error  '));
+
     final service = APIService(uno);
 
     expect(service.getProducts(), completion([]));
